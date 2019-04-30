45c777c813debf4d2e9eaa0ff7bfdad86f423f53
Will Price <will.price94@gmail.com>
2019-04-30 23:32:43 +0100

    Add flowty top-level CLI tests

--- src/flowty/flowty.py
+++ src/flowty/flowty.py
@@ -3,6 +3,7 @@ from flowty.algorithms import tvl1
 import sys
 
 parser = argparse.ArgumentParser(
+        prog='flowty',
         description="Compute optical flow",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
@@ -10,17 +11,19 @@ command_parsers = parser.add_subparsers()
 tvl1.TvL1FlowCommand.register_command(command_parsers)
 
 
-def main(args=None):
-    if args is None:
-        args = parser.parse_args()
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    args = parser.parse_args(argv)
 
     if not hasattr(args, 'command'):
         parser.print_help()
-        sys.exit(1)
+        return 1
 
     command = args.command(args)
     command.main()
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
