b573a4d66c071174e7fa08f9b1cdb3609e191771
Will Price <will.price94@gmail.com>
2019-05-06 09:13:31 +0100

    Rewrite property tests into parameterized ones

--- tests/unit/cv/test_cuda_optflow.py
+++ tests/unit/cv/test_cuda_optflow.py
@@ -1,4 +1,6 @@
 import pytest
+from pytest import approx
+
 import flowty
 import numpy as np
 
@@ -21,35 +23,21 @@ class TestCudaTvL1OpticalFlow(OpticalFlowAlgorithmTestBase):
     def get_flow_algorithm(self):
         return CudaTvL1OpticalFlow()
 
-    def test_tau_property(self):
-        assert CudaTvL1OpticalFlow().tau == 0.25
 
-    def test_lambda_property(self):
-        assert CudaTvL1OpticalFlow().lambda_ == 0.15
-
-    def test_theta_propertys(self):
-        assert CudaTvL1OpticalFlow().theta == 0.3
-
-    def test_epsilon_property(self):
-        assert CudaTvL1OpticalFlow().epsilon == 0.01
-
-    def test_gamma_property(self):
-        assert CudaTvL1OpticalFlow().gamma == 0.0
-
-    def test_scale_step_property(self):
-        assert CudaTvL1OpticalFlow().scale_step == 0.8
-
-    def test_scale_count_property(self):
-        assert CudaTvL1OpticalFlow().scale_count == 5
-
-    def test_warp_count_property(self):
-        assert CudaTvL1OpticalFlow().warp_count == 5
-
-    def test_iterations_property(self):
-        assert CudaTvL1OpticalFlow().iterations == 300
-
-    def test_use_initial_flow_property(self):
-        assert not CudaTvL1OpticalFlow().use_initial_flow
+    @pytest.mark.parametrize("property,expected_value", [
+        ("tau", 0.25),
+        ("lambda_", 0.15),
+        ("theta", 0.3),
+        ("epsilon", 0.01),
+        ("gamma", 0.0),
+        ("scale_step", 0.8),
+        ("scale_count", 5),
+        ("warp_count", 5),
+        ("iterations", 300),
+        ("use_initial_flow", False)
+    ])
+    def test_property(self, property, expected_value):
+        assert getattr(self.get_flow_algorithm(), property) == expected_value
 
     def test_repr(self):
         assert repr(CudaTvL1OpticalFlow()) == ("CudaTvL1OpticalFlow(" +
@@ -62,30 +50,16 @@ class TestCudaBroxOpticalFlow(OpticalFlowAlgorithmTestBase):
     def get_flow_algorithm(self):
         return CudaBroxOpticalFlow()
 
-    def test_alpha_property(self):
-        alpha = 0.5
-        alg = CudaBroxOpticalFlow(alpha=alpha)
-        assert alg.alpha == alpha
-
-    def test_gamma_property(self):
-        gamma = 40.0
-        alg = CudaBroxOpticalFlow(gamma=gamma)
-        assert alg.gamma == gamma
-
-    def test_inner_iterations_property(self):
-        iterations = 100.0
-        alg = CudaBroxOpticalFlow(inner_iterations=iterations)
-        assert alg.inner_iterations == iterations
-
-    def test_outer_iterations_property(self):
-        iterations = 100.0
-        alg = CudaBroxOpticalFlow(outer_iterations=iterations)
-        assert alg.outer_iterations == iterations
-
-    def test_solver_iterations_property(self):
-        iterations = 100.0
-        alg = CudaBroxOpticalFlow(solver_iterations=iterations)
-        assert alg.solver_iterations == iterations
+    @pytest.mark.parametrize("property,expected_value", [
+        ("alpha", approx(0.197)),
+        ("gamma", 50.0),
+        ("scale_factor", approx(0.8)),
+        ("inner_iterations", 5),
+        ("outer_iterations", 150),
+        ("solver_iterations", 10),
+    ])
+    def test_property(self, property, expected_value):
+        assert getattr(self.get_flow_algorithm(), property) == expected_value
 
     def test_repr(self):
         assert repr(CudaBroxOpticalFlow(alpha=1.0, scale_factor=0.8)) == \
@@ -97,15 +71,13 @@ class TestCudaPyramidalLucasKanade(OpticalFlowAlgorithmTestBase):
     def get_flow_algorithm(self):
         return CudaPyramidalLucasKanade()
 
-    def test_window_size_property(self):
-        assert CudaPyramidalLucasKanade().window_size == 13
-
-    def test_max_scales_property(self):
-        assert CudaPyramidalLucasKanade().max_scales == 3
-
-    def test_iterations_property(self):
-        assert CudaPyramidalLucasKanade().iterations == 30
-
+    @pytest.mark.parametrize("property,expected_value", [
+        ("window_size", 13),
+        ("max_scales", 3),
+        ("iterations", 30),
+    ])
+    def test_property(self, property, expected_value):
+        assert getattr(self.get_flow_algorithm(), property) == expected_value
 
 
 class TestCudaFarnebackOpticalFlow(OpticalFlowAlgorithmTestBase):
--- tests/unit/cv/test_optflow.py
+++ tests/unit/cv/test_optflow.py
@@ -62,41 +62,22 @@ class TestTvL1OpticalFlow(OpticalFlowAlgorithmTestBase):
     def get_flow_algorithm(self):
         return TvL1OpticalFlow()
 
-    def test_tau_property(self):
-        assert TvL1OpticalFlow().tau == 0.25
-
-    def test_lambda_property(self):
-        assert TvL1OpticalFlow().lambda_ == 0.15
-
-    def test_theta_propertys(self):
-        assert TvL1OpticalFlow().theta == 0.3
-
-    def test_epsilon_property(self):
-        assert TvL1OpticalFlow().epsilon == 0.01
-
-    def test_gamma_property(self):
-        assert TvL1OpticalFlow().gamma == 0.0
-
-    def test_scale_step_property(self):
-        assert TvL1OpticalFlow().scale_step == 0.8
-
-    def test_scale_count_property(self):
-        assert TvL1OpticalFlow().scale_count == 5
-
-    def test_warp_count_property(self):
-        assert TvL1OpticalFlow().warp_count == 5
-
-    def test_outer_iterations_property(self):
-        assert TvL1OpticalFlow().outer_iterations == 10
-
-    def test_inner_iterations_property(self):
-        assert TvL1OpticalFlow().inner_iterations == 30
-
-    def test_median_filtering_property(self):
-        assert TvL1OpticalFlow().median_filtering == 5
-
-    def test_use_initial_flow_property(self):
-        assert not TvL1OpticalFlow().use_initial_flow
+    @pytest.mark.parametrize("property,expected_value", [
+        ("tau", 0.25),
+        ("lambda_", 0.15),
+        ("theta", 0.3),
+        ("epsilon", 0.01),
+        ("gamma", 0.0),
+        ("scale_step", 0.8),
+        ("scale_count", 5),
+        ("warp_count", 5),
+        ("outer_iterations", 10),
+        ("inner_iterations", 30),
+        ("median_filtering", 5),
+        ("use_initial_flow", False)
+    ])
+    def test_property(self, property, expected_value):
+        assert getattr(self.get_flow_algorithm(), property) == expected_value
 
     def test_repr(self):
         assert repr(TvL1OpticalFlow()) == \
